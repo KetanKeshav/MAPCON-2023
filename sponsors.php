a90cffa94a6d45090c4216d135292405f4f72d55
Ketan Keshav <ketan.keshav7@gmail.com>
2023-01-27 22:03:05 +0530

    Initial Commit

---
+++ sponsors.php
@@ -0,0 +1,193 @@
+  <!-- sponsors section -->
+
+  <section class="about_section layout_padding">
+      <div class="container  ">
+          <div class="heading_container">
+              <h2 style="text-align: center;"><span>Sponsors</span></h2>
+          </div>
+          <div class="row">
+              <div class="col-md-4 img-box mt-2 mb-2">
+                  <a href="https://www.ansys.com/en-in"><img src="https://ieeemapcon.org/images/sponsors/1n.png" alt=""><br><br>
+                      <h2 style="text-align: center;">Signature</h2>
+                  </a>
+              </div>
+          </div>
+          <div class="row">
+              <div class="col-md-4 img-box mt-2 mb-2">
+                  <a href="https://apctechnologies.in/"><img src="https://ieeemapcon.org/images/sponsors/d.png" alt=""><br><br>
+                      <h2 style="text-align: center;">Diamond</h2>
+                  </a>
+              </div>
+              <div class="col-md-4 img-box mt-2 mb-2">
+                  <a href="http://www.renesas.com/"><img src="https://ieeemapcon.org/images/sponsors/d2.png" alt=""><br><br>
+                      <h2 style="text-align: center;">Diamond</h2>
+                  </a>
+              </div>
+              <div class="col-md-4 img-box mt-2 mb-2">
+                  <a href="https://www.mvg-world.com/en"><img src="https://ieeemapcon.org/images/sponsors/diamond.png" alt=""><br><br>
+                      <h2 style="text-align: center;">Diamond</h2>
+                  </a>
+              </div>
+          </div>
+          <div class="row">
+              <div class="col-md-4 img-box mt-2 mb-2">
+                  <a href=""><img src="https://ieeemapcon.org/images/sponsors/p.png" alt=""><br><br>
+                      <h2 style="text-align: center;">Platinum</h2>
+                  </a>
+              </div>
+              <div class="col-md-4 img-box mt-2 mb-2">
+                  <a href="https://www.anritsu.com/en-au/"><img src="https://ieeemapcon.org/images/sponsors/mapcon-logo-sponsor.png" alt=""><br><br>
+                      <h2 style="text-align: center;">Platinum</h2>
+                  </a>
+              </div>
+          </div>
+          <div class="row">
+              <div class="col-md-4 img-box mt-2 mb-2">
+                  <a href="http://www.micross.com/"><img src="https://ieeemapcon.org/images/sponsors/gs.png" alt=""><br><br>
+                      <h2 style="text-align: center;">Gold</h2>
+                  </a>
+              </div>
+              <div class="col-md-4 img-box mt-2 mb-2">
+                  <a href="https://www.dmcrf.com/"><img src="https://ieeemapcon.org/images/sponsors/dmcrf.png" alt=""><br><br>
+                      <h2 style="text-align: center;">Gold</h2>
+                  </a>
+              </div>
+              <div class="col-md-4 img-box mt-2 mb-2">
+                  <a href="http://www.centumelectronics.com/"><img src="https://ieeemapcon.org/images/sponsors/centum.png" alt=""><br><br>
+                      <h2 style="text-align: center;">Gold</h2>
+                  </a>
+              </div>
+              <div class="col-md-4 img-box mt-2 mb-2">
+                  <a href="https://www.sabic.com/en"><img src="https://ieeemapcon.org/images/sponsors/gold-sabik.png" alt=""><br><br>
+                      <h2 style="text-align: center;">Gold</h2>
+                  </a>
+              </div>
+
+              <div class="col-md-4 img-box mt-2 mb-2">
+                  <a href="#"><img src="https://ieeemapcon.org/images/sponsors/gold-rhode.png" alt=""><br><br>
+                      <h2 style="text-align: center;">Gold</h2>
+                  </a>
+              </div>
+
+              <div class="col-md-4 img-box mt-2 mb-2">
+                  <a href="#"><img src="https://ieeemapcon.org/images/sponsors/gold-sponsor-jv.png" alt=""><br><br>
+                      <h2 style="text-align: center;">Gold</h2>
+                  </a>
+              </div>
+          </div>
+          <div class="row">
+              <div class="col-md-4 img-box mt-2 mb-2">
+                  <a href="https://www.minicircuits.com/"><img src="https://ieeemapcon.org/images/sponsors/silver-mclicon.png" alt=""><br><br>
+                      <h2 style="text-align: center;">Silver</h2>
+                  </a>
+              </div>
+              <div class="col-md-4 img-box mt-2 mb-2">
+                  <a href="https://www.airbus.com/en"><img src="https://ieeemapcon.org/images/sponsors/airbus-silver.png" alt=""><br><br>
+                      <h2 style="text-align: center;">Silver</h2>
+                  </a>
+              </div>
+              <div class="col-md-4 img-box mt-2 mb-2">
+                  <a href="https://www.albatross-projects.com/en/"><img src="https://ieeemapcon.org/images/sponsors/silver-sponsor-ap.png" alt=""><br><br>
+                      <h2 style="text-align: center;">Silver</h2>
+                  </a>
+              </div>
+              <div class="col-md-4 img-box mt-2 mb-2">
+                  <a href=""><img src="https://ieeemapcon.org/images/sponsors/altair.png" alt=""><br><br>
+                      <h2 style="text-align: center;">Silver</h2>
+                  </a>
+              </div>
+          </div>
+          <div class="row">
+              <div class="col-md-4 img-box mt-2 mb-2">
+                  <a href="https://www.maurymw.com/"><img src="https://ieeemapcon.org/images/sponsors/bs1.png" alt=""><br><br>
+                      <h2 style="text-align: center;">Bronze</h2>
+                  </a>
+              </div>
+              <div class="col-md-4 img-box mt-2 mb-2">
+                  <a href="https://www.fastech-india.com/"><img src="https://ieeemapcon.org/images/sponsors/bs2.png" alt=""><br><br>
+                      <h2 style="text-align: center;">Bronze</h2>
+                  </a>
+              </div>
+              <div class="col-md-4 img-box mt-2 mb-2">
+                  <a href="https://www.finelineindia.com/"><img src="https://ieeemapcon.org/images/sponsors/bs3.png" alt=""><br><br>
+                      <h2 style="text-align: center;">Bronze</h2>
+                  </a>
+              </div>
+              <div class="col-md-4 img-box mt-2 mb-2">
+                  <a href="https://www.pacemakersolutions.co.in/"><img src="https://ieeemapcon.org/images/sponsors/pacemaker-bronze.png" alt=""><br><br>
+                      <h2 style="text-align: center;">Bronze</h2>
+                  </a>
+              </div>
+              <div class="col-md-4 img-box mt-2 mb-2">
+                  <a href="https://www.anokiwave.com/index.php"><img src="https://ieeemapcon.org/images/sponsors/bronze-awrec.png" alt=""><br><br>
+                      <h2 style="text-align: center;">Bronze</h2>
+                  </a>
+              </div>
+              <div class="col-md-4 img-box mt-2 mb-2">
+                  <a href=""><img src="https://ieeemapcon.org/images/sponsors/bronze-satom.png" alt=""><br><br>
+                      <h2 style="text-align: center;">Bronze</h2>
+                  </a>
+              </div>
+              <div class="col-md-4 img-box mt-2 mb-2">
+                  <a href="https://ssdpolymers.com/"><img src="https://ieeemapcon.org/images/sponsors/ssd-bronze.png" alt=""><br><br>
+                      <h2 style="text-align: center;">Bronze</h2>
+                  </a>
+              </div>
+              <div class="col-md-4 img-box mt-2 mb-2">
+                  <a href="https://www.wavepro.com.tw/"><img src="https://ieeemapcon.org/images/sponsors/wave-bronze.png" alt=""><br><br>
+                      <h2 style="text-align: center;">Bronze</h2>
+                  </a>
+              </div>
+              <div class="col-md-4 img-box mt-2 mb-2">
+                  <a href="https://ieee-aess.org/"><img src="https://ieeemapcon.org/images/sponsors/aess-bronze.png" alt=""><br><br>
+                      <h2 style="text-align: center;">Bronze</h2>
+                  </a>
+              </div>
+              <div class="col-md-4 img-box mt-2 mb-2">
+                  <a href="#"><img src="https://ieeemapcon.org/images/sponsors/bronze-sponsor-cd.png" alt=""><br><br>
+                      <h2 style="text-align: center;">Bronze</h2>
+                  </a>
+              </div>
+              <div class="col-md-4 img-box mt-2 mb-2">
+                  <a href="https://www.jyotimicrosystems.com/"><img src="https://ieeemapcon.org/images/sponsors/bronze-sponsor-ds.png" alt=""><br><br>
+                      <h2 style="text-align: center;">Bronze</h2>
+                  </a>
+              </div>
+              <div class="col-md-4 img-box mt-2 mb-2">
+                  <a href="https://www.astramwp.com/"><img src="https://ieeemapcon.org/images/sponsors/astra.png" alt=""><br><br>
+                      <h2 style="text-align: center;">Bronze</h2>
+                  </a>
+              </div>
+              <div class="col-md-4 img-box mt-2 mb-2">
+                  <a href="#"><img src="https://ieeemapcon.org/images/sponsors/nkrf.png" alt=""><br><br>
+                      <h2 style="text-align: center;">Bronze</h2>
+                  </a>
+              </div>
+              <div class="col-md-4 img-box mt-2 mb-2">
+                  <a href="#"><img src="https://ieeemapcon.org/images/sponsors/comsol.png" alt=""><br><br>
+                      <h2 style="text-align: center;">Bronze</h2>
+                  </a>
+              </div>
+              <div class="col-md-4 img-box mt-2 mb-2">
+                  <a href="#"><img src="https://ieeemapcon.org/images/sponsors/drdo.png" alt=""><br><br>
+                      <h2 style="text-align: center;">Bronze</h2>
+                  </a>
+              </div>
+          </div>
+          <div class="row">
+              <div class="col-md-4 img-box mt-2 mb-2">
+                  <a href="https://www.reva.edu.in/"><img src="https://ieeemapcon.org/images/sponsors/2n.png" alt=""><br><br>
+                      <h2 style="text-align: center;">Academic Partner</h2>
+                  </a>
+              </div>
+              <div class="col-md-4 img-box mt-2 mb-2">
+                  <a href="https://www.microwavejournal.com/"><img src="https://ieeemapcon.org/images/sponsors/3n.png" alt=""><br><br>
+                      <h2 style="text-align: center;">Media Partner</h2>
+                  </a>
+              </div>
+
+          </div>
+      </div>
+  </section>
+
+  <!-- end sponsprs section -->
