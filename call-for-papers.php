5b8577adcaf49c7a7dfecae0a20fb45e65b579ef
anzar00 <peerzada.anzar@gmail.com>
2023-02-12 21:09:00 +0530

    cfa

---
+++ call-for-papers.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html>
+<?php $page = 'cfa'; ?>
+
+<head>
+    <!-- Site Metas -->
+    <meta name="keywords" content="" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+
+    <title>Call For Papers  | MAPCON 2023</title>
+
+    <?php include 'include/head.php' ?>
+
+</head>
+
+<body class="sub_page">
+    <div class="hero_area">
+        <!-- header section strats -->
+        <header class="header_section">
+            <div class="container-fluid">
+                <?php include 'include/navbar.php'; ?>
+            </div>
+        </header>
+    </div>
+    <!-- end header section -->
+
+    <section class="course_section layout_padding content-bg">
+        <div class="container">
+            <div class="heading_container heading_center">
+                <h2>
+                    CALL FOR <span>PAPERS</span>
+                </h2>
+            </div>
+            <hr style="border-bottom: 3px solid #2575fc; width: 50%;">
+            <div class="col-md-12">
+                <img src="images/uc.png" class="img-fluid" alt="Under Construction" width="100%">
+            </div>
+            <!-- 
+                <div class="cfa_heading">
+        <h1>Dear <span>Researcher</span>,</h1>
+        <p style="text-align: justify;">
+          On behalf of IEEE MAPCON 2022 Organizing Committeee, I take this opportunity to invite you to participate in
+          this professionally enriching flagship technical conference namely IEEE MAPCON. IEEE MAPCON is joint
+          initiative of IEEE Antenna and Propagation Society (APS) and IEEE Microwave Theory & Techniques Society
+          (MTT-S). 1st edition of IEEE MAPCON will be organized on December 12-15,2022. MAPCON 2022 will have
+          technical tracks covering all the domains of Microwaves, Antennas and Propagation.
+          The eminent International speakers from US, Europe and premier National research and Industrial Organizations
+          will deliver talks and conduct workshops and tutorials. During this international conference, the prospective
+          researchers will have the opportunity to present their own work in the form of oral presentation, apart from
+          taking the advantage of the talks by a number of international experts. You are requested to upload your
+          paper having length of min 4 pages to maximum six pages, to paper submission portal:
+          <br><br>
+          <a href=" https://edas.info/N29753"> https://edas.info/N29753</a>
+          <br><br>
+          I assure you of an international technically enriched and vibrant experience at MAPCON 2022, and I
+          am sure your experience at this conference will be a rewarding and memorable one.
+
+          <br><br><br>
+
+          <strong>
+            Puneet Kumar Mishra and Mrinal Kanti Mandal,<br>
+            General Chairs, IEEE MAPCON 2022
+          </strong>
+        </p>
+      </div>
+             -->
+<!-- 
+            
+
+-->
+
+
+        </div>
+    </section>
+
+
+    <!-- sponsors section -->
+
+    <?php include 'sponsors.php'; ?>
+
+    <!-- end sponsprs section -->
+
+    <?php include 'include/footer.php'; ?>
+
+
+</body>
+
+</html>
--- include/navbar.php
+++ include/navbar.php
@@ -31,21 +31,21 @@
                         Program
                     </a>
                     <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <a class="dropdown-item <?php if ($page == "tutorial") echo "active" ?>" href="./tutorial.php">Tutorial</a>
-                        <a class="dropdown-item <?php if ($page == "ppl") echo "active" ?>" href="./program/Pathak_lectures.php">Prof. Pathak lectures</a>
-                        <a class="dropdown-item <?php if ($page == "w1") echo "active" ?>" href="workshop-1.php">Workshop-1</a>
-                        <a class="dropdown-item <?php if ($page == "w2") echo "active" ?>" href="workshop-2.php">Workshop-2</a>
+                        <!-- <a class="dropdown-item <?php if ($page == "tutorial") echo "active" ?>" href="./tutorial.php">Tutorial</a> -->
+                        <!-- <a class="dropdown-item <?php if ($page == "ppl") echo "active" ?>" href="./program/Pathak_lectures.php">Prof. Pathak lectures</a> -->
+                        <!-- <a class="dropdown-item <?php if ($page == "w1") echo "active" ?>" href="workshop-1.php">Workshop-1</a> -->
+                        <!-- <a class="dropdown-item <?php if ($page == "w2") echo "active" ?>" href="workshop-2.php">Workshop-2</a> -->
                         <a class="dropdown-item <?php if ($page == "program") echo "active" ?>" href="program.php">Conference</a>
-                        <a class="dropdown-item <?php if ($page == "6g") echo "active" ?>" href="./program/6g.php">Special Sessions on 6G</a>
-                        <a class="dropdown-item <?php if ($page == "aa") echo "active" ?>" href="./program/antenna.php">Antenna Arrays</a>
-                        <a class="dropdown-item <?php if ($page == "gan") echo "active" ?>" href="./program/gan.php">GaN ASIC</a>
+                        <!-- <a class="dropdown-item <?php if ($page == "6g") echo "active" ?>" href="./program/6g.php">Special Sessions on 6G</a> -->
+                        <!-- <a class="dropdown-item <?php if ($page == "aa") echo "active" ?>" href="./program/antenna.php">Antenna Arrays</a> -->
+                        <!-- <a class="dropdown-item <?php if ($page == "gan") echo "active" ?>" href="./program/gan.php">GaN ASIC</a> -->
                         <a class="dropdown-item <?php if ($page == "kn") echo "active" ?>" href="./program/keynotes.php">Keynotes</a>
-                        <a class="dropdown-item <?php if ($page == "tt") echo "active" ?>" href="./program/tedtalk.php">Banquet TED Talk</a>
+                        <!-- <a class="dropdown-item <?php if ($page == "tt") echo "active" ?>" href="./program/tedtalk.php">Banquet TED Talk</a> -->
                         <a class="dropdown-item <?php if ($page == "yp") echo "active" ?>" href="./program/yp.php">YP Activities</a>
                         <a class="dropdown-item <?php if ($page == "wie") echo "active" ?>" href="./program/wie.php">WIE Activities</a>
                         <a class="dropdown-item <?php if ($page == "ap") echo "active" ?>" href="./program/ap.php">AP-S & MTT-S SIGHT</a>
-                        <a class="dropdown-item" href="downloads/MAPCON Presentation.pptx" target="_blank">Presentation
-                            Template</a>
+                        <!-- <a class="dropdown-item" href="downloads/MAPCON Presentation.pptx" target="_blank">Presentation
+                            Template</a> -->
                         <!-- <a class="dropdown-item" href="downloads/MAPCON_digital_poster.pptx" target="_blank">Poster Template (A0 size only)</a> -->
                     </div>
                 </li>
@@ -55,7 +55,7 @@
                     </a>
                     <div class="dropdown-menu" aria-labelledby="navbarDropdown">
                         <!-- <a class="dropdown-item <?php if ($page == "ra") echo "active" ?>" href="./registered-authors.php">Registered Authors</a> -->
-                        <a class="dropdown-item" href="./call-for-papers/index.php">Call For Papers</a>
+                        <a class="dropdown-item" href="call-for-papers.php">Call For Papers</a>
                         <a class="dropdown-item" href="./paper-submission/index.php">Paper Submission</a>
                         <a class="dropdown-item" href="important-dates.php">Important Dates</a>
                         <a class="dropdown-item <?php if ($page == "awards") echo "active" ?>" href="awards.php">Awards</a>
