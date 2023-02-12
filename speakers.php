366dac40f384010896e08e261c81221577d8a643
anzar00 <peerzada.anzar@gmail.com>
2023-02-12 20:18:33 +0530

    speakers

---
+++ speakers.php
@@ -0,0 +1,264 @@
+<!DOCTYPE html>
+<html>
+<?php $page = "tpc"; ?>
+
+<head>
+    <!-- Site Metas -->
+    <meta name="keywords" content="" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+
+    <title>Speakers | MAPCON 2023</title>
+
+    <?php include 'include/head.php' ?>
+
+    <style>
+        .card {
+            margin-top: 20px;
+            border: none;
+            background-color: transparent;
+        }
+
+        .card-title {
+            font-size: 20px;
+            font-weight: bold;
+        }
+
+        .card-text {
+            text-align: left;
+            color: #473F3F;
+            font-family: 'Red Hat Text', Sans-serif;
+            font-size: 18px;
+        }
+
+        .card-img-top {
+            width: 300px;
+            height: 325px;
+        }
+
+        .span_head {
+            font-weight: bold;
+            color: #EF4D48;
+        }
+    </style>
+</head>
+
+<body class="sub_page">
+    <div class="hero_area">
+        <!-- header section strats -->
+        <header class="header_section">
+            <div class="container-fluid">
+                <?php include 'include/navbar.php'; ?>
+            </div>
+        </header>
+    </div>
+    <!-- end header section -->
+
+    <section class="course_section layout_padding content-bg">
+        <div class="container">
+            <div class="heading_container heading_center">
+                <h2>
+                    <span>Speakers</span>
+                </h2>
+            </div>
+            <hr style="border-bottom: 3px solid #2575fc; width: 50%;">
+            <div class="col-md-12">
+                <img src="images/uc.png" class="img-fluid" alt="Under Construction" width="100%">
+            </div>
+            <!-- <div class="heading_container span_body">
+                <div class="row d-flex justify-content-center">
+                    <div class="card col-lg-6 col-md-6">
+                        <center>
+                            <img class="card-img-top" src="./images/speakers/mos.png" alt="Card image">
+                        </center>
+                        <div class="card-body">
+                            <center>
+                                <h4 class="card-title">Shri. Rajeev Chandrasekhar</h4>
+                                <p class="card-text">Union Minister of State for Ministry of Electronics &
+                                    Information Technology and Skill Development and Entrepreneurship</p>
+                            </center>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+
+                    <div class="card col-lg-4 col-md-4 col-sm-6">
+                        <center>
+                            <img class="card-img-top" src="./images/speakers/somanath.webp" alt="Card image">
+                        </center>
+                        <div class="card-body">
+                            <h4 class="card-title">S. Somanath</h4>
+                            <p class="card-text">Secretary, DoS / Chairman ISRO</p>
+                        </div>
+                    </div>
+                    <div class="card col-lg-4 col-md-4 col-sm-6">
+                        <center>
+                            <img class="card-img-top" src="./images/speakers/satheesh.webp" alt="Card image">
+                        </center>
+                        <div class="card-body">
+                            <h4 class="card-title">G. Satheesh Reddy</h4>
+                            <p class="card-text">Secretary, DDR&D/ Chairman, DRDO</p>
+                        </div>
+                    </div>
+                    <div class="card col-lg-4 col-md-4 col-sm-6">
+                        <center>
+                            <img class="card-img-top" src="./images/speakers/rahman.jpg" alt="Card image">
+                        </center>
+                        <div class="card-body">
+                            <h4 class="card-title">Prof. Saifur Rahman</h4>
+                            <p class="card-text">2022 President-Elect IEEE</p>
+                        </div>
+                    </div>
+                    <div class="card col-lg-4 col-md-4 col-sm-6">
+                        <center>
+                            <img class="card-img-top" src="./images/speakers/sankaran.png" alt="Card image">
+                        </center>
+                        <div class="card-body">
+                            <h4 class="card-title">M Sankaran</h4>
+                            <p class="card-text">Director, URSC, ISRO</p>
+                        </div>
+                    </div>
+                    <div class="card col-lg-4 col-md-4 col-sm-6">
+                        <center>
+                            <img class="card-img-top" src="./images/speakers/LRDE-Director.jpg" alt="Card image">
+                        </center>
+                        <div class="card-body">
+                            <h4 class="card-title">Dr. P Radhakrishna</h4>
+                            <p class="card-text">Director, LRDE, DRDO</p>
+                        </div>
+                    </div>
+                    <div class="card col-lg-4 col-md-4 col-sm-6">
+                        <center>
+                            <img class="card-img-top" src="./images/speakers/rashaunda.jpg" alt="Card image">
+                        </center>
+                        <div class="card-body">
+                            <h4 class="card-title">Prof. Rashaunda Henderson</h4>
+                            <p class="card-text">IEEE MTT-S President</p>
+                        </div>
+                    </div>
+                    <div class="card col-lg-4 col-md-4 col-sm-6">
+                        <center>
+                            <img class="card-img-top" src="./images/speakers/sherry.jpg" alt="Card image">
+                        </center>
+                        <div class="card-body">
+                            <h4 class="card-title">Sherry Hess</h4>
+                            <p class="card-text">WiM Chair, IEEE MTT-S</p>
+                        </div>
+                    </div>
+                    <div class="card col-lg-4 col-md-4 col-sm-6">
+                        <center>
+                            <img class="card-img-top" src="./images/speakers/prabhakar.jpg" alt="Card image">
+                        </center>
+                        <div class="card-body">
+                            <h4 class="card-title">Prabhakar H. Pathak</h4>
+                            <p class="card-text">Prof. Emeritus at OSU</p>
+                        </div>
+                    </div>
+                    <div class="card col-lg-4 col-md-4 col-sm-6">
+                        <center>
+                            <img class="card-img-top" src="./images/speakers/john.jpg" alt="Card image">
+                        </center>
+                        <div class="card-body">
+                            <h4 class="card-title">Prof. John Volakis</h4>
+                            <p class="card-text">Florida International University</p>
+                        </div>
+                    </div>
+                    <div class="card col-lg-4 col-md-4 col-sm-6">
+                        <center>
+                            <img class="card-img-top" src="./images/speakers/Giuliano.jpg" alt="Card image">
+                        </center>
+                        <div class="card-body">
+                            <h4 class="card-title">Prof. Giuliano Manara</h4>
+                            <p class="card-text">University of Pisa</p>
+                        </div>
+                    </div>
+                    <div class="card col-lg-4 col-md-4 col-sm-6">
+                        <center>
+                            <img class="card-img-top" src="./images/speakers/Stefano.jpg" alt="Card image">
+                        </center>
+                        <div class="card-body">
+                            <h4 class="card-title">Prof. Stefano Maci</h4>
+                            <p class="card-text">IEEE AP-S President-Elect</p>
+                        </div>
+                    </div>
+                    <div class="card col-lg-4 col-md-4 col-sm-6">
+                        <center>
+                            <img class="card-img-top" src="./images/speakers/Arun.jpg" alt="Card image">
+                        </center>
+                        <div class="card-body">
+                            <h4 class="card-title">Dr. Arun K Bhattacharyya</h4>
+                            <p class="card-text">Northrop Grumann</p>
+                        </div>
+                    </div>
+                    <div class="card col-lg-4 col-md-4 col-sm-6">
+                        <center>
+                            <img class="card-img-top" src="./images/speakers/Debashis.jpg" alt="Card image">
+                        </center>
+                        <div class="card-body">
+                            <h4 class="card-title">Prof. Debashis Chanda</h4>
+                            <p class="card-text">Uni. Of Central Florida</p>
+                        </div>
+                    </div>
+                    <div class="card col-lg-4 col-md-4 col-sm-6">
+                        <center>
+                            <img class="card-img-top" src="./images/speakers/Rajlakshmi.jpg" alt="Card image">
+                        </center>
+                        <div class="card-body">
+                            <h4 class="card-title">Dr. Rajlakshmi Menon</h4>
+                            <p class="card-text">Director, CABS, DRDO</p>
+                        </div>
+                    </div>
+                    <div class="card col-lg-4 col-md-4 col-sm-6">
+                        <center>
+                            <img class="card-img-top" src="./images/speakers/Sungtek.jpg" alt="Card image">
+                        </center>
+                        <div class="card-body">
+                            <h4 class="card-title">Prof. Sungtek Kahng</h4>
+                            <p class="card-text">Incheon National University</p>
+                        </div>
+                    </div>
+                    <div class="card col-lg-4 col-md-4 col-sm-6">
+                        <center>
+                            <img class="card-img-top" src="./images/speakers/amir.jpg" alt="Card image">
+                        </center>
+                        <div class="card-body">
+                            <h4 class="card-title">Prof. Amir Boag</h4>
+                            <p class="card-text">Tel Aviv University</p>
+                        </div>
+                    </div>
+                    <div class="card col-lg-4 col-md-4 col-sm-6">
+                        <center>
+                            <img class="card-img-top" src="./images/speakers/jiro.jpg" alt="Card image">
+                        </center>
+                        <div class="card-body">
+                            <h4 class="card-title">Prof. Jiro Hirokawa</h4>
+                            <p class="card-text">Tokyo Institute of Technology</p>
+                        </div>
+                    </div>
+                    <div class="card col-lg-4 col-md-4 col-sm-6">
+                        <center>
+                            <img class="card-img-top" src="./images/speakers/Madhumita.png" alt="Card image">
+                        </center>
+                        <div class="card-body">
+                            <h4 class="card-title">Dr. Madhumita Chakravarti</h4>
+                            <p class="card-text"> Director, CMSDS</p>
+                        </div>
+                    </div>
+                </div>
+            </div> -->
+        </div>
+    </section>
+
+
+    <!-- sponsors section -->
+
+    <?php include 'sponsors.php'; ?>
+
+    <!-- end sponsprs section -->
+
+    <?php include 'include/footer.php'; ?>
+
+
+</body>
+
+</html>
