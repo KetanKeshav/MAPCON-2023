11ecfae061036c326c4678191b119060027e6049
anzar00 <peerzada.anzar@gmail.com>
2023-02-12 21:01:37 +0530

    job-opportunities

---
+++ job-opportunities.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html>
+<?php $page = 'job-opportunities'; ?>
+
+<head>
+    <!-- Site Metas -->
+    <meta name="keywords" content="" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+
+    <title>Job Opportunities | MAPCON 2023</title>
+
+    <?php include 'include/head.php' ?>
+
+</head>
+
+<body class="sub_page">
+    <div class="hero_area">
+        <!-- header section strats -->
+        <header class="header_section">
+            <div class="container-fluid">
+                <?php include 'include/navbar.php'; ?>
+            </div>
+        </header>
+    </div>
+    <!-- end header section -->
+
+    <section class="course_section layout_padding content-bg">
+        <div class="container">
+            <div class="heading_container heading_center">
+                <h2>
+                    Job Opportunities - <span>MAPCON 2022</span>
+                </h2>
+            </div>
+            <hr style="border-bottom: 3px solid #2575fc; width: 50%;">
+            <div class="col-md-12">
+                <img src="images/uc.png" class="img-fluid" alt="Under Construction" width="100%">
+            </div>
+            <!-- <div class="cfa_heading">
+                <p style="text-align: justify;">
+                    Dear Professor,
+                    <br><br>
+                    Greetings from the IEEE MAPCON 2022 Team! We have created a portal to gather the possible research
+                    and job opportunities (both short term and long term) in RF, Microwave and Antennas domain in India.
+                    <br><br>
+                    The purpose of this initiative is to promote research and jobs in RF and Microwave field, by
+                    providing a single consolidated page containing valuable information for students and Young
+                    Professionals working in this research domain.
+                    <br><br>
+                    Please take this opportunity to advertise the openings in your group starting January 2023, by
+                    filling up the following Google form by November 30, 2022: <a href="https://docs.google.com/forms/d/1jbUCUGom-mJwZ94dwxsDOpvWNjBYBN9TOE-7olFUp2Q
+            ">Fill the Form </a>
+                    <br><br>
+                    We are planning to compile the information and share it with students and YPs during the conference.
+                    <br><br>
+                    Looking forward to hearing from you soon.
+                    <br><br>
+                    Thanks and Best Regards,
+                    <br><br>
+                    <b>
+                        Debdeep Sarkar (Co-Chair, YP Committee, IEEE MAPCON 2022)<br>
+                        Somak Bhattacharyya (Co-Chair, YP Committee, IEEE MAPCON 2022)<br>
+                        Puneet Kumar Mishra (General Chair, IEEE MAPCON 2022)<br>
+                        Web: https://www.ieeemapcon.org/
+                    </b>
+
+
+
+
+
+                </p>
+            </div>
+            <center>
+                <a href="https://docs.google.com/forms/d/1jbUCUGom-mJwZ94dwxsDOpvWNjBYBN9TOE-7olFUp2Q" style="border: 1px solid #ffffff;
+      display: inline-block;
+      padding: 10px 65px;
+      background-color: #EF4D48;
+      color: #ffffff;
+      border-radius: 35px;
+      margin-top: 15px;">Apply Now</a>
+            </center> -->
+
+
+        </div>
+    </section>
+
+
+    <!-- sponsors section -->
+
+    <?php include 'sponsors.php'; ?>
+
+    <!-- end sponsprs section -->
+
+    <?php include 'include/footer.php'; ?>
+
+
+</body>
+
+</html>
