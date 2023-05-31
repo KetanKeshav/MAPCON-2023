7b84b97a006755a9a0a57f8b337989dfd0a5dd1c
anzar00 <peerzada.anzar@gmail.com>
2023-06-01 01:05:50 +0530

    imp dates - submission link added

--- important-dates.php
+++ important-dates.php
@@ -57,9 +57,14 @@
 
                 </div>
             </div>
+            <div class="submit-area">
+        <center >
+            <a href="https://edas.info/newPaper.php?c=31083" target="_blank">Submit Paper</a>
+        </center>     
+    </div>
         </section>
     </div>
-
+    
     <!-- sponsors section -->
 
     <?php //include 'sponsors.php'; ?>
