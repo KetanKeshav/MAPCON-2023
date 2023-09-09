b395f6a08bece4b0a75087f22109d94efd08ce3e
Ketan Keshav <ketan.keshav7@gmail.com>
2023-09-09 12:33:38 +0530

    PhD update

--- index.php
+++ index.php
@@ -91,7 +91,7 @@
         <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h1 class="modal-title display-4">Paper Submission Deadline Extended!</h1>
+                    <h1 class="modal-title display-4">PhD Student Initiative</h1>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
